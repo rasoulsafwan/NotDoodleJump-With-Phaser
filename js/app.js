ae3bee3c6a7d705e3d0d9f16855c2f97cf41a786
Safwan <4iq4.com@gmail.com>
2024-07-12 20:14:40 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
-var config = {
-    type: Phaser.AUTO,
-    scale : {
-        parent: 'gamespace',
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 600,
-        height: 800
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            // debug: true
-        }
-    },
-    scene: [Game, ],
-    backgroundColor: 0xeeeeee
-};
-// console.log(game.config.scale);
-var game = new Phaser.Game(config);
+    var config = {
+        type: Phaser.AUTO,
+        scale : {
+            parent: 'gamespace',
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+            width: 600,
+            height: 800
+        },
+        physics: {
+            default: 'arcade',
+            arcade: {
+                // debug: true
+            }
+        },
+        scene: [Game, ],
+        backgroundColor: 0xeeeeee
+    };
+    // console.log(game.config.scale);
+    var game = new Phaser.Game(config);
 
